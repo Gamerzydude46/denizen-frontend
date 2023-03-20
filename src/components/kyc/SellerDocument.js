bb230c081002e50f1fe9436da0648b55d828515d
Kalpa07 <kalpateli0711@gmail.com>
2023-03-21 01:21:23 +0530

    changes in ui of kyc

--- src/components/kyc/SellerDocument.js
+++ src/components/kyc/SellerDocument.js
@@ -4,6 +4,7 @@ import FormControl from '@mui/material/FormControl';
 import dummyImg2 from '../../assets/images/dummyImg2.png';
 import { ThemeProvider } from "@mui/material/styles";
 import { createTheme } from "@mui/material/styles";
+import { Typography } from "@mui/material";
 
     const CustomFontTheme = createTheme({
         typography: {
@@ -34,12 +35,12 @@ import { createTheme } from "@mui/material/styles";
          <FormControl variant="standard" >
             <form className='mt-5 ml-14'>
                 <ThemeProvider theme={CustomFontTheme}>
-                    <div className="flex flex-row gap-10">
+                    <div className="flex flex-row gap-10 ">
                     <div className='mt-2'>
                         <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                         <div style={{
                         width:' 60%',minwidth: '450px',position: 'absolute',top:' 40%',
-                        left: '15%',borderradius: '7px'}}>
+                        left: '10%',borderradius: '7px'}}>
                         <div className="img-holder" style={{width:'120px',heigth:'120px'}}>
 						<img src={dummyImg2} alt="" id="img" className="img" style={{ width: '120px', height: '120px' }}/>
 					    </div>
@@ -52,10 +53,10 @@ import { createTheme } from "@mui/material/styles";
                         </div>
                         </Box>
                     </div>
-                    <div  className='mt-16 display-flex justify-content gap-20 ml-20'>
+                    <div  className='mt-16 display-flex justify-content gap-15 ml-20'>
                        
-                            <div style={{ display:'flex',marginLeft:'50px',gap:'0%'}} >
-                            <label>Business/Shop Registration Documents</label>
+                            <div style={{ display:'flex',marginLeft:'50px',gap:'-1%'}} >
+                            <Typography sx={{fontWeight:'550' }}>Business/Shop Registration Documents</Typography>
                          
                             <input type="file" id="id1" hidden="yes" />
                             <label for='id1' className='flex justify-center ml-5 documentButton text-oswald w-[120px]   '>
@@ -63,8 +64,8 @@ import { createTheme } from "@mui/material/styles";
                             <i class="fas fa-upload"></i> &nbsp; Browse
                             </label>
                             </div>
-                            <div style={{ display:'flex',marginLeft:'50px',marginTop:'12px',gap:'9%'}} >
-                            <label>Registered Lease/Rent  Agreement</label>
+                            <div style={{ display:'flex',marginLeft:'50px',marginTop:'12px',gap:'9.3%'}} >
+                            <Typography sx={{fontWeight:'550' }}>Registered Lease/Rent  Agreement</Typography>
                          
                             <input type="file" id="id1" hidden="yes" />
                             <label for='id1' className='flex justify-center ml-4 documentButton text-oswald w-[120px]   '>
@@ -73,11 +74,11 @@ import { createTheme } from "@mui/material/styles";
                             </label>
                             </div>
                            
-                            <div style={{ display:'flex',marginLeft:'50px',marginTop:'12px',gap:'18%'}} >
-                            <label>Resident Ceritificate</label>
+                            <div style={{ display:'flex',marginLeft:'50px',marginTop:'12px',gap:'18.5%'}} >
+                            <Typography  sx={{fontWeight:'550' }} >Resident Ceritificate</Typography>
                          
                             <input type="file" id="id1" hidden="yes" />
-                            <label for='id1' className='flex justify-center ml-20 documentButton text-oswald w-[120px] h-  '>
+                            <label for='id1' className='flex justify-center ml-20 documentButton text-oswald w-[120px]  '>
                                 
                             <i class="fas fa-upload"></i> &nbsp; Browse
                             </label>
