4b757fe1c2652e2bceb1f40a62f7ea9adb240740
sujayKanolkar <it.seven@maxxcell.in>
2023-04-25 18:44:30 +0530

    web3.straoges setup

---
+++ src/services/postItems.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+
+export const createItem = async (
+    item_name,
+    delivery_address,
+    item_cost,
+    delivery_cost,
+    distance,
+    delivery_date,
+    delivery_by,
+    category,
+    URL,
+    imageName,
+) => {
+    var config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:8080/postItems/post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: {
+            "item_name": item_name,
+            "delivery_address": delivery_address,
+            "item_cost": item_cost,
+            "delivery_cost": delivery_cost,
+            "distance": distance,
+            "delivery_date": delivery_date,
+            "delivery_by": delivery_by,
+            "category": category,
+            "imageURL":  {
+                "URL" : URL,
+                "name": imageName,
+            },
+        }
+    };
+
+    try{
+        const response = await axios.post(config.url,config.data,{withCredentials: true})
+        return (response);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
